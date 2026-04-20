24d20372b79c7090b99d84183b57678bca0db7f6
Lounes Ounouz <lounesounouz@gmail.com>
2026-04-20 22:24:30 +0200

    fixing

--- src/ilu2/ChiffreRomain.java
+++ src/ilu2/ChiffreRomain.java
@@ -1,31 +1,49 @@
 package ilu2;
 
 public class ChiffreRomain {
-	private ChiffreRomain chiffre;
-	private static final String[] SYMBOLES = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-
+//	private ChiffreRomain chiffre;
+//	private static final String[] SYMBOLES = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+//
+//	public String toChiffresRomains(int nombre) {
+//		if (nombre <= 0 || nombre > 3999) {
+//			throw new IllegalArgumentException();
+//		}
+//		if (nombre == 4) {
+//			return SYMBOLES[11];
+//		}
+//		if (nombre == 9) {
+//			return "IX";
+//		}
+//		StringBuilder st = new StringBuilder();
+//		if (nombre >= 5) {
+//			st.append("V");
+//			nombre = nombre - 5;
+//		}
+////		if (nombre <= 3) {
+//		for (int i = 0; i < nombre; i++) {
+//			st.append(SYMBOLES[12]);
+//		}
+////		}
+////		else if ()
+//		return st.toString();
+//	}
 	public String toChiffresRomains(int nombre) {
 		if (nombre <= 0 || nombre > 3999) {
 			throw new IllegalArgumentException();
 		}
-		if (nombre == 4) {
-			return SYMBOLES[11];
-		}
-		if (nombre == 9) {
-			return "IX";
-		}
-		StringBuilder st = new StringBuilder();
-		if (nombre >= 5) {
-			st.append("V");
-			nombre = nombre - 5;
-		}
-//		if (nombre <= 3) {
-		for (int i = 0; i < nombre; i++) {
-			st.append(SYMBOLES[12]);
-		}
-//		}
-//		else if ()
-		return st.toString();
-	}
 
+		StringBuilder sb = new StringBuilder();
+
+		int[] valeurs = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+		String[] symboles = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+		for (int i = 0; i < valeurs.length; i++) {
+			while (nombre >= valeurs[i]) {
+				sb.append(symboles[i]);
+				nombre -= valeurs[i];
+			}
+		}
+
+		return sb.toString();
+	}
 }
